94e7c7db5066f110568cba8bc2434b01c80759e6
RamaSandhy <rshandy07@gmail.com>
2025-07-02 00:55:05 +0700

    menambahkan file homepage dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'features/travel/presentation/pages/search_page.dart';
+import 'travelkita/features/travel/presentation/pages/home_page.dart';
 
 void main() {
   runApp(const TravelKitaApp());
@@ -15,46 +15,7 @@ class TravelKitaApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SearchPage(),
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Setelah 3 detik pindah ke halaman pencarian
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => const SearchPage()),
-      );
-    });
-
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/.png', 
-              height: 200,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Selamat datang di TravelKita',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Colors.blueAccent,
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
