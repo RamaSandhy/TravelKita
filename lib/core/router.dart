f577e0b55f0df97e241b196cb2b04d1987431a02
RamaSandhy <rshandy07@gmail.com>
2025-07-10 10:36:35 +0700

    perbaikan router

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,15 +1,29 @@
-// core/router.dart
 import 'package:flutter/material.dart';
+import 'package:travelkita/features/auth/presentation/pages/login_page.dart';
 import 'package:travelkita/features/travel/domain/entities/jadwal_travel.dart';
-import 'package:travelkita/features/travel/presentation/pages/booking_page.dart';
-import 'package:travelkita/features/travel/presentation/pages/detail_travel_page.dart';
-import 'package:travelkita/features/travel/presentation/pages/home_page.dart';
+import 'package:travelkita/features/travel/presentation/user_pages/home_page.dart';
+import 'package:travelkita/features/travel/presentation/user_pages/booking_page.dart';
+import 'package:travelkita/features/travel/presentation/user_pages/detail_travel_page.dart';
+import 'package:travelkita/features/travel/presentation/user_pages/home_menu_page.dart'; // ini halaman awal
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => const HomePage());
+        return MaterialPageRoute(builder: (_) =>  LoginPage());
+
+      // User
+      case '/menu':
+        return MaterialPageRoute(builder: (_) => const HomeMenuPage());
+
+      case '/home':
+        return MaterialPageRoute(builder: (_) => const HomePage()); // ← tambahkan ini
+
+      case '/detail':
+        final jadwal = settings.arguments as JadwalTravel;
+        return MaterialPageRoute(
+          builder: (_) => DetailTravelPage(jadwalTravel: jadwal),
+        );
 
       case '/booking':
         final jadwal = settings.arguments as JadwalTravel;
@@ -17,11 +31,7 @@ class AppRouter {
           builder: (_) => BookingPage(jadwalTravel: jadwal),
         );
 
-      case '/detail':
-        final jadwal = settings.arguments as JadwalTravel;
-        return MaterialPageRoute(
-          builder: (_) => DetailTravelPage(jadwalTravel: jadwal),
-        );
+      // Admin
 
       default:
         return MaterialPageRoute(
