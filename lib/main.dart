fcf21daa549d4c9bb0ce3017300f1fadfa6f6d5c
RamaSandhy <rshandy07@gmail.com>
2025-07-10 10:08:35 +0700

    perbaikan

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travelkita/features/auth/bloc/login_bloc.dart';
+import 'package:travelkita/features/auth/data/repositories/auth_repository.dart';
 import 'core/router.dart';
 import 'features/auth/presentation/pages/login_page.dart';
-import 'features/travel/presentation/pages/home_page.dart';
+import 'features/travel/presentation/user_pages/home_page.dart';
 
 void main() {
   runApp(const TravelKitaApp());
@@ -12,10 +15,17 @@ class TravelKitaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TravelKita',
-      initialRoute: '/',
-      onGenerateRoute: AppRouter.generateRoute,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => AuthBloc(repository: AuthRepository()),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TravelKita',
+        initialRoute: '/',
+        onGenerateRoute: AppRouter.generateRoute,
+      ),
     );
   }
 }
