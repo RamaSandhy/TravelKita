c2eedb0713cdc689d4ac660325fa416d176d1f5a
RamaSandhy <rshandy07@gmail.com>
2025-07-14 05:58:03 +0700

    menambahkan http client

---
+++ lib/service/service_htp_client.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+
+class ServiceHttpClient {
+  final String baseUrl = 'http://10.0.2.2:8000/api/';
+  final secureStorage = FlutterSecureStorage();
+
+  //Post Data
+  Future<http.Response> post(String endPoint, Map<String, dynamic> body) async {
+    final url = Uri.parse('$baseUrl$endPoint');
+    try{
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+    }catch(e){
+      throw Exception('Error in post request: $e');
+    }
+  }
+
+  //Post Data with Token
+  Future<http.Response> postWithToken(
+    String endPoint, 
+    Map<String, dynamic> body
+    ) async {
+      final url = Uri.parse('$baseUrl$endPoint');
+      final token = await secureStorage.read(key: 'token'); 
+      try{
+        final response = await http.post(
+          url,
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+          body: jsonEncode(body),
+        );
+        return response;
+      }catch(e){
+        throw Exception('Error in post request: $e');
+      }
+  }
+
+  Future<http.Response> get(String endPoint) async {
+  final url = Uri.parse('$baseUrl$endPoint');
+  final token = await secureStorage.read(key: 'token');
+  return await http.get(
+    url,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    },
+  );
+}
+}
